01e716ec4551298107b832f05406bc30ee5cf853
rahmathadiyatul <rahmathadiyatulakbar@gmail.com>
2023-11-26 21:50:47 +0700

    Footer finished

--- src/3_Footer/Footer.js
+++ src/3_Footer/Footer.js
@@ -1,229 +1,92 @@
 import React from 'react';
 import { Box, ImageList, ImageListItem } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 import '@fontsource/poppins';
-
-
+import { LocationOn, Call, Email, Facebook, LinkedIn, Twitter, YouTube, Instagram } from '@mui/icons-material';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <Box sx={{
-                display: 'flex',
-                marginTop: 0,
-                bottom: 0,
-                position: 'relative',
-                flexDirection: 'row',
-                justifyContent: 'space-around',
-                boxSizing: 'border-box',
-                backgroundColor: '#05014a',
-                height: '220px',
-                padding: '24px 95px',
-                flexWrap: 'wrap',
-            }}>
-                <Box
-                    sx={{
-                        width: '30%',
-                        background: 'rgba(0,0,0,0)',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'flex-start'
-                    }}>
-                    <Typography sx={{
-                        fontFamily: 'Poppins',
-                        fontStyle: 'normal',
-                        fontWeight: 500,
-                        fontSize: '16px',
-                        lineHeight: '24px',
-                        color: 'white',
-                        background: 'rgba(0,0,0,0)',
-                        textAlign: 'justify',
-                        marginBottom: '10px'
-                    }}>About Us</Typography>
-                    <Typography sx={{
-                        background: 'rgba(0,0,0,0)',
-                        fontFamily: 'Poppins',
-                        fontStyle: 'normal',
-                        fontWeight: 400,
-                        fontSize: '14px',
-                        lineHeight: '21px',
-                        textAlign: 'justify',
-                        color: '#FFFFFF'
-                    }}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </Typography>
-                </Box>
-                <Box sx={{
-                    background: 'rgba(0,0,0,0)',
-                    displax: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'flex-start',
-                    width: '20%'
-                }}>
-                    <Typography sx={{
-                        fontFamily: 'Poppins',
-                        textAlign: 'left',
-                        fontStyle: 'normal',
-                        fontWeight: 500,
-                        fontSize: '16px',
-                        lineHeight: '24px',
-                        color: 'white',
-                        background: 'rgba(0,0,0,0)',
-                    }}>Akcel</Typography>
-                    <ul style={{
-                        marginTop: '17px',
-                        marginLeft: '25px',
-                        alignItems: 'flex-start',
-                        gap: '10px',
-                        maxHeight: '140px',
-                        width: '200px',
-                        display: 'flex',
-                        flexFlow: 'column wrap',
-                        background: 'rgba(0,0,0,0)',
-                        fontFamily: 'Poppins',
-                        fontStyle: 'normal',
-                        fontWeight: '400',
-                        fontSize: '14px',
-                        textAlign: 'justify',
-                        color: '#FFFFFF',
-                    }}>
-                        <li style={{
-                            background: 'rgba(0,0,0,0)'
-                        }}>Why Akcel</li>
-                        <li style={{
-                            background: 'rgba(0,0,0,0)'
-                        }}>Enterprise</li>
-                        <li style={{
-                            background: 'rgba(0,0,0,0)'
-
-                        }}>Customer Story</li>
-                        <li style={{
-                            background: 'rgba(0,0,0,0)'
-
-                        }}>Security</li>
-                        <li style={{
-                            background: 'rgba(0,0,0,0)'
-
-                        }}>Pricing</li>
-                    </ul>
-                </Box>
-                <Box sx={{
-                    width: '30%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'flex-start',
-                    background: 'rgba(0,0,0,0)',
-                    gap: '20px',
-                }}>
-                    <Box sx={{
-                        background: 'rgba(0,0,0,0)',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'flex-start',
-                        gap: '10px'
-                    }}>
-                        <Typography sx={{
-                            fontFamily: 'Poppins',
-                            fontStyle: 'normal',
-                            fontWeight: 500,
-                            fontSize: '16px',
-                            lineHeight: '24px',
-                            color: 'white',
-                            background: 'rgba(0,0,0,0)',
-                        }}>Address</Typography>
-                        <Typography sx={{
-                            background: 'rgba(0,0,0,0)',
-                            fontFamily: 'Poppins',
-                            fontStyle: 'normal',
-                            fontWeight: 400,
-                            fontSize: '14px',
-                            lineHeight: '21px',
-                            textAlign: 'justify',
-                            color: '#FFFFFF'
-                        }}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</Typography>
+        <div className='footer'>
+            <Box class='main-footer'>
+                <Box class='footer-1'>
+                    <Box class='full-logo'>
+                        <Link to='/'>
+                            <img
+                                className='logo'
+                                src="https://res.cloudinary.com/dxyxg3egs/image/upload/v1700152396/bossku/logo_od1hnl.png"
+                                alt="logo"
+                            />
+                        </Link>
+                        <div className='logo-text'>
+                            <h1 className="boss">BOSSKU</h1>
+                            <Typography class='sme'>SME’s Capitalization</Typography>
+                        </div>
                     </Box>
-                    <Box sx={{
-                        background: 'rgba(0,0,0,0)',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'flex-start',
-
-                    }}>
-                        <Typography sx={{
-                            fontFamily: 'Poppins',
-                            fontStyle: 'normal',
-                            fontWeight: 500,
-                            fontSize: '16px',
-                            lineHeight: '24px',
-                            color: 'white',
-                            background: 'rgba(0,0,0,0)',
-                        }}>Contact Us</Typography>
-                        <Box sx={{
-                            background: 'rgba(0,0,0,0)'
-                        }}>
-                            <ImageList sx={{
-                                background: 'rgba(0,0,0,0)',
-                                display: 'flex',
-                                flexDirection: 'row',
-                            }}>
-                                <ImageListItem sx={{
-                                    background: 'rgba(0,0,0,0)',
-                                    width: '48px',
-                                    maxHeight: '48px',
-                                    marginTop: '7px',
-                                    marginRight: '10px'
-                                }}>
-                                    <img alt='phone' style={{
-                                        background: 'rgba(0,0,0,0)'
-                                    }} src='https://res.cloudinary.com/dxyxg3egs/image/upload/v1666031100/SoupProject/1-removebg-preview_k2zcbk.png'></img>
-                                </ImageListItem>
-                                <ImageListItem sx={{
-                                    background: 'rgba(0,0,0,0)',
-                                    width: '48px',
-                                    maxHeight: '48px',
-                                    marginTop: '7px',
-                                    marginRight: '10px'
-                                }}>
-                                    <img alt='instagram' style={{
-                                        background: 'rgba(0,0,0,0)'
-                                    }} src='https://res.cloudinary.com/dxyxg3egs/image/upload/v1666031100/SoupProject/2-removebg-preview_b2oukf.png'></img>
-                                </ImageListItem>
-                                <ImageListItem sx={{
-                                    background: 'rgba(0,0,0,0)',
-                                    width: '48px',
-                                    maxHeight: '48px',
-                                    marginTop: '7px',
-                                    marginRight: '10px'
-                                }}>
-                                    <img alt='youtube' style={{
-                                        background: 'rgba(0,0,0,0)'
-                                    }} src='https://res.cloudinary.com/dxyxg3egs/image/upload/v1666031100/SoupProject/3-removebg-preview_pynukn.png'></img>
-                                </ImageListItem>
-                                <ImageListItem sx={{
-                                    background: 'rgba(0,0,0,0)',
-                                    width: '48px',
-                                    maxHeight: '48px',
-                                    marginTop: '7px',
-                                    marginRight: '10px'
-                                }}>
-                                    <img alt='telegram' style={{
-                                        background: 'rgba(0,0,0,0)'
-                                    }} src='https://res.cloudinary.com/dxyxg3egs/image/upload/v1666031100/SoupProject/4-removebg-preview_gdj7sp.png'></img>
-                                </ImageListItem>
-                                <ImageListItem sx={{
-                                    background: 'rgba(0,0,0,0)',
-                                    width: '48px',
-                                    maxHeight: '48px',
-                                    marginTop: '7px',
-                                    marginRight: '10px'
-                                }}>
-                                    <img alt='mail' style={{
-                                        background: 'rgba(0,0,0,0)'
-                                    }} src='https://res.cloudinary.com/dxyxg3egs/image/upload/v1666031100/SoupProject/5-removebg-preview_aqry7f.png'></img>
-                                </ImageListItem>
-                            </ImageList>
-                        </Box>
+                    <Typography class='footer-par'>Bossku empowerment, a driving force that fuels the dreams and aspirations of Small and Medium Enterprises (SMEs) across Indonesia. "Bossku,"</Typography>
+                    <Box class='media-icons'>
+                        <div>
+                            <Facebook></Facebook>
+                        </div>
+                        <div>
+                            <Twitter></Twitter>
+                        </div>
+                        <div>
+                            <YouTube></YouTube>
+                        </div>
+                        <div>
+                            <LinkedIn></LinkedIn>
+                        </div>
+                        <div>
+                            <Instagram></Instagram>
+                        </div>
+                    </Box>
+                </Box>
+                <Box class='footer-2'>
+                    <Typography class='header'>Akcel</Typography>
+                    <Typography class='data'>Why Akcel</Typography>
+                    <Typography class='data'>Enterprise</Typography>
+                    <Typography class='data'>Customer Story</Typography>
+                    <Typography class='data'>Security</Typography>
+                    <Typography class='data'>Pricing</Typography>
+                </Box>
+                <Box class='footer-2'>
+                    <Typography class='header'>Resources</Typography>
+                    <Typography class='data'>Download</Typography>
+                    <Typography class='data'>Help Center</Typography>
+                    <Typography class='data'>Events</Typography>
+                    <Typography class='data'>Guides</Typography>
+                    <Typography class='data'>Partner</Typography>
+                    <Typography class='data'>Directories</Typography>
+                </Box>
+                <Box class='footer-2'>
+                    <Typography class='header'>Company</Typography>
+                    <Typography class='data'>About Us</Typography>
+                    <Typography class='data'>Contact Us</Typography>
+                    <Typography class='data'>Products</Typography>
+                    <Typography class='data'>Login</Typography>
+                    <Typography class='data'>Sign Up</Typography>
+                    <Typography class='data'>FAQ</Typography>
+                </Box>
+                <Box class='footer-3'>
+                    <Typography class='header'>Get in Touch with Us</Typography>
+                    <Box class='footer-contacts'>
+                        <LocationOn></LocationOn>
+                        <Typography class='data'>832  Thompson Drive, San Fransisco<br></br>
+                            CA 94107, United States</Typography>
+                    </Box>
+                    <Box class='footer-contacts'>
+                        <Call></Call>
+                        <Typography class='data'>394-091-3312</Typography>
+                    </Box>
+                    <Box class='footer-contacts'>
+                        <Email></Email>
+                        <Typography class='data'>support@akcel.com</Typography>
                     </Box>
                 </Box>
             </Box>
+            <Typography class='copyright'>PT. Bossku Bossmu Juga  -  © 2023 Bossku.id</Typography>
         </div >
     )
 }
