58505725687a3d4a4223a92e5bcda7bea15616e6
rahmathadiyatul <rahmathadiyatulakbar@gmail.com>
2023-11-26 22:31:08 +0700

    Header finished

--- src/1_Header/Header.js
+++ src/1_Header/Header.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
 import { Button, Dropdown } from '@mui/base';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Email, Call, ExitToApp } from '@mui/icons-material';
 
 function Header() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -13,7 +14,37 @@ function Header() {
   };
 
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Box sx={{ margin: '1em 4em 0 4em', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Box sx={{ alignItems: 'center', display: 'flex', flexDirection: 'row', gap: '1.5em' }}>
+          <Typography class='top-header'>Your dreams. Your venture. You are The  Boss.</Typography>
+          <Button style={{
+            display: 'flex',
+            flexDirection: 'row',
+            height: '2.5em',
+            borderRadius: '8px',
+            background: 'var(--Changes---Main, #BD0304)',
+            alignItems: 'center',
+            border: 'none',
+            color: 'white',
+            padding: '0 2em'
+
+          }}>
+            <ExitToApp></ExitToApp>
+            <Typography>Sign Up Now</Typography>
+          </Button>
+        </Box>
+        <Box sx={{ alignItems: 'center', display: 'flex', flexDirection: 'row', gap: '1.5em' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: '.3em', color: '#BD0304', fontFamily: 'sans-serif', alignItems: 'center' }}>
+            <Call sx={{ height: '.7em' }}></Call>
+            <Typography sx={{ fontSize: '.8em' }} class='top-texts'>394-091-3312</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: '.3em', color: '#BD0304', fontFamily: 'sans-serif', alignItems: 'center' }}>
+            <Email sx={{ height: '.7em' }}></Email>
+            <Typography sx={{ fontSize: '.8em' }} class='top-texts'>help@bossku.id</Typography>
+          </Box>
+        </Box>
+      </Box>
       <header>
         <div className='full-logo'>
           <Link to='/'>
@@ -24,8 +55,8 @@ function Header() {
             />
           </Link>
           <div className='logo-text'>
-            <h1 className="boss">BOSSKU</h1>
-            <Typography class='sme'>SME’s Capitalization</Typography>
+            <h1 className="bosses">BOSSKU</h1>
+            <Typography class='smes'>SME’s Capitalization</Typography>
           </div>
         </div>
         <nav>
@@ -44,13 +75,28 @@ function Header() {
                 )}
               </a>
             </li>
-            <li><Link to='/invoice'>Notifications</Link></li>
             <li><Link to='/invoice'>About</Link></li>
             <li><Link to='/invoice'>Contact Us</Link></li>
             <li className='account'>
-              <Button>
-                <Link to='/invoice'>
-                  <PersonIcon sx={{ color: '#BD0304' }} className='person-icon'></PersonIcon>
+              <Button style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '.8em .8em',
+                gap: '13px',
+                borderRadius: '8px',
+                height: '3em',
+                border: '1px solid var(--primary-700, #BD0304)',
+                transition: 'background 0.3s ease, transform 0.2s ease, box-shadow 0.3s ease'
+              }}>
+                <Link style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }} to='/invoice'>
+                  <PersonIcon sx={{ color: '#BD0304', height: '1em' }} className='person-icon'></PersonIcon>
                   <Typography class='my-account'>My Account</Typography>
                 </Link>
               </Button>
@@ -58,7 +104,7 @@ function Header() {
           </ul>
         </nav>
       </header>
-    </div>
+    </div >
   );
 }
 
